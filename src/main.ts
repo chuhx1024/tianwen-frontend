81b44163663fc9eaf53acda9597f4a52c3099e68
chuhx <chuhx1024@126.com>
2023-12-11 15:31:22 +0800

    feat: add antd-vue

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+import '@/assets/styles/index.scss'
+
 const app = createApp(App)
 
 app.use(createPinia())
